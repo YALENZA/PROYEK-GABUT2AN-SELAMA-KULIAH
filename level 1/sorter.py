572f36378b94eb98d5b97a22627147dc0c17347a
YALENZA <araara91pku@gmail.com>
2026-06-25 15:38:54 +0700

    nambah dictionary biar filenya masuk berdasarkan ekstensi yang dibutuhkan

--- level 1/sorter.py
+++ level 1/sorter.py
@@ -1,14 +1,30 @@
 import os
 
-# ngmabil lokasi
+# dictionary untuk menyimpan ekstensi file dan folder tujuan
+sortingan_folder = {
+    ".jpg": "images",
+    ".png": "images",
+    ".pdf": "documents",
+    ".docx": "documents",
+    ".txt": "Notes",
+    ".mp4": "Videos"
+}
+
+# ngamabil lokasi sekarang
 lokasi_sekarang = os.getcwd()
 
 # baca isi folder
 isi_folder = os.listdir(lokasi_sekarang)
 
 for item in isi_folder:
-
     if os.path.isfile(item):
+        # misahin nama file dan ekstensinya 
         nama, ekstensi = os.path.splitext(item)
         print("nama", nama, "| ekstensi:", ekstensi)
+
+        # pencegahan jika ekstensi tidak ada di dictionary
+        folder_tujuan = sortingan_folder.get(ekstensi, "lainnya")
+        print(f"File {item} akan dipindahkan ke folder {folder_tujuan}")
+
+
 
