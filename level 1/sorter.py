a7e187d120c53a63c1edcc3710e5948c3d26e5c2
YALENZA <araara91pku@gmail.com>
2026-06-25 15:53:39 +0700

    FINAL: proyek dipakai untuk memindahkan isi file berdasarkan ekstensinya ke folder yang dituju

--- level 1/sorter.py
+++ level 1/sorter.py
@@ -1,4 +1,5 @@
-import os
+import os #library buat akses sistem operasi
+import shutil # library buat mindahin file
 
 # dictionary untuk menyimpan ekstensi file dan folder tujuan
 sortingan_folder = {
@@ -20,10 +21,18 @@ for item in isi_folder:
     if os.path.isfile(item):
         # misahin nama file dan ekstensinya 
         nama, ekstensi = os.path.splitext(item)
-        print("nama", nama, "| ekstensi:", ekstensi)
 
         # pencegahan jika ekstensi tidak ada di dictionary
         folder_tujuan = sortingan_folder.get(ekstensi, "lainnya")
+        # ngebuat folder baru kalo folder tujuan belum ada
+        os.makedirs(folder_tujuan, exist_ok=True)
+        # ngegabungin lokasi sekarang sama nama file buat alamat asal dan tujuan
+        alamat_asal = os.path.join(lokasi_sekarang, item)
+        # ngegabungin lokasi sekarang sama nama folder tujuan sama nama file buat alamat tujuan
+        alamat_tujuan = os.path.join(lokasi_sekarang, folder_tujuan, item)
+        # perpindahan file
+        shutil.move(alamat_asal, alamat_tujuan)
+
         print(f"File {item} akan dipindahkan ke folder {folder_tujuan}")
 
 
