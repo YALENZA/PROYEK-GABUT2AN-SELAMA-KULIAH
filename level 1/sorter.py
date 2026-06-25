dd6c1db3c7c013bbec5e2c75c4dc46e349f456f6
YALENZA <araara91pku@gmail.com>
2026-06-25 15:09:30 +0700

    progress awal untuk sorter.py

--- level 1/sorter.py
+++ level 1/sorter.py
@@ -1,1 +1,14 @@
-print("TEST DOANG")
+import os
+
+# ngmabil lokasi
+lokasi_sekarang = os.getcwd()
+
+# baca isi folder
+isi_folder = os.listdir(lokasi_sekarang)
+
+for item in isi_folder:
+
+    if os.path.isfile(item):
+        nama, ekstensi = os.path.splitext(item)
+        print("nama", nama, "| ekstensi:", ekstensi)
+
