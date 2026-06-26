a9a40bfcb5d5c691f325a91d3c44f1c2ef66cb78
YALENZA <araara91pku@gmail.com>
2026-06-26 16:26:01 +0700

    okei sudah sesuai fungsinya (kurang lebih)

--- level 1/pass_generator.py
+++ level 1/pass_generator.py
@@ -1,30 +1,39 @@
 import string
 import random
 
+# karakter nerima semua huruf alfabet (besar dan kecil)
 karakter = string.ascii_letters 
 
+# bisa pakai angka
 pakai_angka = input("Gunakan angka? (Y/N):").upper()
 if pakai_angka == "Y":
     karakter += string.digits
 
+#bisa pakai simbol
 pakai_simbol = input("Gunakan simbol? (Y/N):").upper()
 if pakai_simbol == "Y":
     karakter += string.punctuation
 
+# sengaja kosong biar ntar bisa bikin pw otomatis (wadahnya) 
 password_final = ""
 
+# fungsi validasi panjang password
 while True:
-    input_panjang = input("masukkan panjang password (min: 8): ")
-    panjang = int(input_panjang)
+    try:
+        input_panjang = input("masukka_genen panjang password (min: 8): ")
+        panjang = int(input_panjang)
 
-    if panjang < 8:
-        print("Panjang password minimal 8 karakter.")
-    else:
-        break
+        if panjang < 8:
+            print("Panjang password minimal 8 karakter.")
+        else:
+            break
+    except ValueError:
+        print("Silakan masukkan angka yang valid.")
 
+# menghasilkan password berdasarkan panjang yang dimasukkan
 for i in range(panjang):
     random_karakter = random.choice(karakter)
     password_final += random_karakter
 
-print("Password yang dihasilkan:", password_final)
+print("Password anda:", password_final)
 
